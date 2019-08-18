2e3670c8a0b9a7503bb9db45f34baa1a4dc791f9
RyogaAoyama <ryogaaoym0114@gmail.com>
2019-08-19 06:14:31 +0900

    アカウント_編集_プロフィール編集テストコード完成

---
+++ spec/support/system/common_helper.rb
@@ -0,0 +1,12 @@
+# 全機能共通ヘルパー
+module CommonHelper
+  def login
+    FactoryBot.create(:secret_question)
+    user = FactoryBot.create(:user)
+    visit login_path
+    fill_in 'login_id', with: user.login_id
+    fill_in 'password', with: user.password
+    click_on 'ログイン'
+    "成功"
+  end
+end
--- spec/system/admin/acount_spec.rb
+++ spec/system/admin/acount_spec.rb
@@ -69,3 +69,104 @@ describe '入力フォームのエラーメッセージが表示される' do
     end
   end
 end
+
+describe 'アカウントの編集' do
+  let(:user) { User.find(1) }
+  before do
+    login
+    click_on 'userName'
+  end
+
+  describe 'プロフィール編集' do
+    before do
+      click_on 'プロフィール編集'
+    end
+
+    context 'エラーパターン' do
+      it '【名前】エラーメッセージが表示される' do
+        fill_in 'name', with: ''
+        click_on '保存'
+        expect(page).to have_content '名前を入力してください'
+      end
+      it '【写真】エラーメッセージが表示される' do
+        attach_file "profile_img", "/tmp/test.txt"
+        click_on '保存'
+        expect(page).to have_content '画像ファイルを選択してください'
+      end
+      it '【名前】入力した値が保持されていること' do
+        fill_in 'name', with: 'a' * 40
+        expect(page).to have_field 'name', with: 'a' * 40
+      end
+    end
+
+    context '正常パターン' do
+      it '登録されている名前が入力されている' do
+        expect(page).to have_field 'name', with: user.name
+      end
+      it '登録されている写真がアップロードされている' do
+        attach_file 'profile_img', 'TODO:写真のパスはこの機能のPGしてからかく'
+        # TODO: have_fieldで画像ファイルが取れるのか確認
+        expect(page).to have_field 'profile_img', with: user.image
+      end
+      it '名前が変更されている' do
+        fill_in 'name', with: '藤井'
+        click_on '保存'
+        expect(user.name).to eq '藤井'
+      end
+      it '写真が変更されている' do
+        attach_file "profile_img", "TODO:写真のパスはこの機能のPGしてからかく"
+        click_on '保存'
+        expect(user.image).to eq 'TODO:写真のパスはこの機能のPGしてからかく'
+      end
+      it 'プロフィール変更完了のメッセージが出力されている' do
+        click_on '保存'
+        expect(page).to have_content '編集内容を保存しました'
+      end
+    end
+
+  end
+  describe 'パスワードの変更' do
+    context '認証' do
+      context 'エラーパターン' do
+        it 'エラーメッセージが表示される'
+      end
+      context '正常パターン' do
+        it 'パスワード変更画面へ遷移している'
+      end
+    end
+
+    context '変更' do
+      context 'エラーパターン' do
+        it 'エラーメッセージが表示される'
+        it 'エラーメッセージが表示される'
+      end
+      context '正常パターン' do
+        it 'パスワードが変更されている'
+        it 'アカウント詳細画面に遷移している'
+        it 'パスワード変更完了のメッセージが出力されている'
+      end
+    end
+    describe '秘密の質問' do
+      context '認証' do
+        context 'エラーパターン' do
+          it 'エラーメッセージが表示される'
+          it '入力した値が保持されていること'
+        end
+        context '正常パターン' do
+          it '秘密の質問変更画面へ遷移している'
+        end
+      end
+  
+      context '変更' do
+        context 'エラーパターン' do
+          it 'エラーメッセージが表示される'
+        end
+        context '正常パターン' do
+          it '秘密の質問が変更されている'
+          it 'アカウント詳細画面に遷移している'
+          it '秘密の質問変更完了のメッセージが出力されている'
+        end
+      end
+    end
+  end
+end
