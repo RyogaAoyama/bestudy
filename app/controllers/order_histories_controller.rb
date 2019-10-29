a8d3a9901e502671923549c58c539a3639f39dad
RyogaAoyama <ryogaaoym0114@gmail.com>
2019-10-30 07:23:12 +0900

    【一般】商品_注文履歴_完成

--- app/controllers/order_histories_controller.rb
+++ app/controllers/order_histories_controller.rb
@@ -1,4 +1,6 @@
+# 注文履歴のコントローラー
 class OrderHistoriesController < ApplicationController
   def index
+    @order_histories = OrderHistory.where(user_id: current_user.id)
   end
 end
--- spec/system/nomal/order_history_spec.rb
+++ spec/system/nomal/order_history_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
-
 describe '履歴機能' do
   let(:room) { FactoryBot.create(:new_room2) }
   let(:product) { FactoryBot.create(:new_product2, room_id: room.id, user_id: room.user_id) }
   let(:user) { FactoryBot.create(:new_nomal_user, room_id: room.id, password: 'testuser1') }
   let(:order_history) { FactoryBot.create(:order_history, user_id: user.id, room_id: room.id, product_id: product.id) }
-  
+
   before do
     order_history
     new_login(user.login_id, 'testuser1')
@@ -17,7 +16,7 @@ describe '履歴機能' do
     expect(page).to have_content product.name
     expect(page).to have_content "#{ product.point }pt"
     expect(page).to have_content "#{ product.created_at.strftime('%Y/%m/%d') }に購入"
-    expect(find('#img')[:src]).to match(/test.jpg/)
+    expect(find("#img-#{ order_history.id }")[:src]).to match(/test.jpg/)
   end
 
   context 'ルームに所属してない場合' do
@@ -28,27 +27,22 @@ describe '履歴機能' do
     end
   end
 
-  context '注文した商品が削除された場合' do
-    let(:product) { FactoryBot.create(:new_product2, room_id: room.id, user_id: room.user_id, is_deleted: true) }
-    it '履歴にメッセージを表示' do
-      expect(page).to have_content 'この商品は削除されました'
-    end
-  end
-  
   context '注文中の商品の場合' do
     it '履歴にメッセージを表示' do
-      expect(page).to have_content '現在、注文中です'
+      expect(page).to have_content '現在、注文中です。'
     end
   end
-  
+
   context '配送が完了した場合' do
-    let(:order_history) { FactoryBot.create(
-      :order_history, 
-      user_id: user.id, 
-      room_id: room.id, 
-      product_id: product.id, 
-      is_order_success: true
-      ) }
+    let(:order_history) do
+      FactoryBot.create(
+        :order_history,
+        user_id: user.id,
+        room_id: room.id,
+        product_id: product.id,
+        is_order_success: true
+      )
+    end
     it '履歴にメッセージを表示' do
       expect(page).to have_content "#{ order_history.updated_at.strftime('%Y/%m/%d') }にお届け完了"
     end
@@ -57,7 +51,7 @@ describe '履歴機能' do
   context '注文がない場合' do
     let(:order_history) { '' }
     it 'メッセージを表示' do
-      expect(page).to have_content '現在、注文はありません'
+      expect(page).to have_content '現在、注文はありません。'
     end
   end
 end
