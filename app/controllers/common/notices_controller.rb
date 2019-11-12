f479c9d2fc707ff1848c98b9200801a910466d91
RyogaAoyama <39504783+RyogaAoyama@users.noreply.github.com>
2019-11-12 22:10:55 +0900

    Revert "お知らせ機能"

--- app/controllers/common/notices_controller.rb
+++ app/controllers/common/notices_controller.rb
@@ -1,11 +1,7 @@
 class Common::NoticesController < ApplicationController
   def index
-    # テーブルのカラムの用途が一般と管理人で違ったため無茶苦茶な条件になった
-    # 必ず次に活かすこと
-    @notices = if current_user.is_admin
-                 Notice.where(type: [3, 4, 5]).where(room_id: owner_room.id)
-               else
-                 Notice.where(type: [1, 2]).where(user_id: current_user.id)
-               end
+  end
+
+  def show
   end
 end
